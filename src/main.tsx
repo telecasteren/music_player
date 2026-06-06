7d987db961c77665aacad6d97c4125e60bdde2d7
telecasteren <127958662+telecasteren@users.noreply.github.com>
2026-06-06 08:51:34 +0200

    Updated TODO list

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import AppWrapper from "./app/app-wrapper";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./css/App.css";
 import "./css/index.css";
-import { ThemeProvider } from "@/components/theme-provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,5 +13,5 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <AppWrapper />
       </ThemeProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
